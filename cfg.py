70e53a4749d4dad4f21280bef27d7b462b7bc19a
Kirfi7 <kirbel357@gmail.com>
2023-06-10 22:38:54 +0500

    фиксы и апдейт

--- cfg.py
+++ cfg.py
@@ -1,8 +1,8 @@
-session = {"sessionid": "7riz0v5bscp2rsqkp6wkmpbbrovwrmky"}
+session = {"sessionid": "fj93t81y2p56dmpvnn8je4hqbt59inoj"}
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
-access = {"1831358099": "Миша", "": "Кирилл"}
+access = {"1831358099": "Миша", "777198928": "Кирилл"}
 
 token = "6133560304:AAGKJFWkCyRroXfd0YYNYl9meIj3ierGv-s"
--- main.py
+++ main.py
@@ -17,12 +17,21 @@ class Jails(StatesGroup):
 class Mutes(StatesGroup):
     date = State()
 
+class Kickes(StatesGroup):
+    date = State()
+
+class Mero(StatesGroup):
+    date = State()
+
+class Bans(StatesGroup):
+    date = State()
+
 
 @dp.message_handler(commands=['jails'])
 async def get_date(message: types.Message):
 
-    if message.from_user.id not in access:
-        return await message.answer("У вас нет доступа к использованию бота!")
+    # if not (message.from_user.id in access):
+    #     return await message.answer("У вас нет доступа к использованию бота!")
 
     await Jails.date.set()
     await message.answer(
@@ -65,8 +74,8 @@ async def get_jails(message: types.Message, state: FSMContext):
 @dp.message_handler(commands=['mutes'])
 async def get_date(message: types.Message):
 
-    if message.from_user.id not in access:
-        return await message.answer("У вас нет доступа к использованию бота!")
+    # if not (message.from_user.id in access):
+    #     return await message.answer("У вас нет доступа к использованию бота!")
 
     await Mutes.date.set()
     await message.answer(
@@ -104,6 +113,141 @@ async def get_mutes(message: types.Message, state: FSMContext):
 
     await state.finish()
     await message.answer(msg)
+
+
+@dp.message_handler(commands=['kickes'])
+async def get_date(message: types.Message):
+
+    # if not (message.from_user.id in access):
+    #     return await message.answer("У вас нет доступа к использованию бота!")
+
+    await Kickes.date.set()
+    await message.answer(
+        "Укажите дату, за которую хотите получить выгрузку в следующем формате: месяц-день. Пример: 01-30"
+    )
+
+
+@dp.message_handler(state=Kickes.date)
+async def get_kickes(message: types.Message, state: FSMContext):
+    await Kickes.date.set()
+    kickes = {}
+    offset = 0
+    while True:
+
+        link = get_link(date=message.text, description='Кикнул', category=41, offset=offset)
+        response = get_response(link)
+
+        for tup in response:
+            if tup["player_name"] in kickes:
+                kickes[tup["player_name"]] += 1
+            else:
+                kickes[tup["player_name"]] = 1
+
+        if len(response) == 200:
+            offset += 200
+            continue
+        break
+
+    msg, k = "", 0
+    top = dict(sorted(kickes.items(), key=lambda item: item[1], reverse=True))
+
+    for key in top:
+        k += 1
+        msg += f"{k}. {key}: {top[key]}\n"
+
+    await state.finish()
+    await message.answer(msg)
+
+
+@dp.message_handler(commands=['mp'])
+async def get_date(message: types.Message):
+
+    # if not (message.from_user.id in access):
+    #     return await message.answer("У вас нет доступа к использованию бота!")
+
+    await Mero.date.set()
+    await message.answer(
+        "Укажите дату, за которую хотите получить выгрузку в следующем формате: месяц-день. Пример: 01-30"
+    )
+
+
+@dp.message_handler(state=Mero.date)
+async def get_mero(message: types.Message, state: FSMContext):
+    await Mero.date.set()
+    mero = {}
+    offset = 0
+    while True:
+
+        link = get_link(date=message.text, description='Создал мероприятие', category=41, offset=offset)
+        response = get_response(link)
+
+        for tup in response:
+            if tup["player_name"] in mero:
+                mero[tup["player_name"]] += 1
+            else:
+                mero[tup["player_name"]] = 1
+
+        if len(response) == 200:
+            offset += 200
+            continue
+        break
+
+    msg, k = "", 0
+    top = dict(sorted(mero.items(), key=lambda item: item[1], reverse=True))
+
+    for key in top:
+        k += 1
+        msg += f"{k}. {key}: {top[key]}\n"
+
+    await state.finish()
+    await message.answer(msg)
+
+
+# @dp.message_handler(commands=['ban'])
+# async def get_date(message: types.Message):
+#
+#     # if not (message.from_user.id in access):
+#     #     return await message.answer("У вас нет доступа к использованию бота!")
+#
+#     await Bans.date.set()
+#     await message.answer(
+#         "Укажите дату, за которую хотите получить выгрузку в следующем формате: месяц-день. Пример: 01-30"
+#     )
+#
+#
+# @dp.message_handler(state=Bans.date)
+# async def get_bans(message: types.Message, state: FSMContext):
+#     await Bans.date.set()
+#     bans = {}
+#     offset = 0
+#     while True:
+#
+#
+#         link = get_link(date=message.text, description='заблокировал', offset=offset)
+#         response = get_response(link)
+#
+#         for tup in response:
+#             if tup["player_name"] in bans:
+#                 bans[tup["player_name"]] += 1
+#             else:
+#                 bans[tup["player_name"]] = 1
+#
+#         if len(response) == 200:
+#             offset += 200
+#             continue
+#         break
+#
+#     msg, k = "", 0
+#     top = dict(sorted(bans.items(), key=lambda item: item[1], reverse=True))
+#
+#     for key in top:
+#         k += 1
+#         msg += f"{k}. {key}: {top[key]}\n"
+#
+#     await state.finish()
+#     await message.answer(msg)
+
+
 
 
 @dp.message_handler(commands=['farm'])
