522d53995c4e3881245c50ed9fca549514c06ce5
Kirfi7 <57867941+Kirfi7@users.noreply.github.com>
2023-08-17 22:58:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from aiogram import Bot, Dispatcher, executor
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+import asyncio
+from aiogram import types
+from aiogram import Bot, Dispatcher, executor
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
 
 from crud import *
 from buttons import *
@@ -173,6 +179,8 @@ async def get_date(message: types.Message, state: FSMContext):
 
 if __name__ == "__main__":
     try:
-        executor.start_polling(dispatcher=dp, skip_updates=True)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(executor.start_polling(dispatcher=dp, skip_updates=True))
+        # executor.start_polling(dispatcher=dp, skip_updates=True)
     except Exception as error:
         print(error)
